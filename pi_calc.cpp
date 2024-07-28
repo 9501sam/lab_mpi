46e8fae7260662a78d61ad87753c0bc26fc15eff
Shi-Huang Tseng <9501sam@gmail.com>
2024-07-28 17:08:41 +0800

    accepted

--- pi_calc.cpp
+++ pi_calc.cpp
@@ -2,30 +2,47 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <mpi.h>  // Include the MPI header
+
 using namespace std;
 
-//TODO: turn it to MPI program!
-int main(int argc, char *argv[])
-{
-    // random seed
-    srand(time(NULL));
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);  // Initialize MPI
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // Get the rank of the process
+    MPI_Comm_size(MPI_COMM_WORLD, &size);  // Get the number of processes
+
+    // Seed the random number generator with a unique seed for each process
+    srand(time(NULL) + rank);
 
     long long circle_cnt = 0, square_cnt = 0;
     long long total_tests = strtoll(argv[1], NULL, 10);
 
-    for (int i = 0; i < total_tests; ++i)
-    {
-        // random num within 0 ~ 1
+    // Distribute the number of tests among processes
+    long long local_tests = total_tests / size;
+    long long local_circle_cnt = 0;
+
+    // Each process performs its local number of tests
+    for (long long i = 0; i < local_tests; ++i) {
         double x = (double)rand() / RAND_MAX;
         double y = (double)rand() / RAND_MAX;
-        
-        double d = x*x + y*y;
-        if(d <= 1) circle_cnt++;
-        square_cnt++;
-    }
-    
-    double pi = 4.0 * circle_cnt / square_cnt;
-    cout << "Pi = " << pi << endl;
 
+        double d = x * x + y * y;
+        if (d <= 1) local_circle_cnt++;
+    }
+
+    // Use MPI_Reduce to sum up all local_circle_cnt to get the global circle count
+    MPI_Reduce(&local_circle_cnt, &circle_cnt, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // Calculate pi in the root process (rank 0)
+    if (rank == 0) {
+        square_cnt = total_tests;  // Total number of square counts
+        double pi = 4.0 * circle_cnt / square_cnt;
+        cout << "Pi = " << pi << endl;
+    }
+
+    MPI_Finalize();  // Finalize MPI
     return 0;
 }
+
